2ea23f455f2e04f0342d8ea3f70fad96fb7cd57d
zhaohui90-lee <fhenn@163.com>
2020-09-01 18:07:14 +0800

    '首页轮播拆分'

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,29 +1,21 @@
 <template>
   <div id="home">
-    <nav-bar class="home-nav">
-      <div slot="center">购物街</div>
-    </nav-bar>
-    <swiper>
-      <swiper-item v-for="item in banners">
-        <a :href="item.link">
-          <img :src="item.image" alt="">
-        </a>
-      </swiper-item>
-    </swiper>
+    <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+    <home-swiper :banners="banners"/>
   </div>
 </template>
 
 <script>
   import NavBar from "../../components/common/navbar/NavBar";
+  import HomeSwiper from "./childComps/HomeSwiper";
+
   import {getHomeMultidata} from "network/home"
-  import {Swiper, SwiperItem} from "components/common/swiper"
 
   export default {
     name: "Home",
     components: {
       NavBar,
-      Swiper,
-      SwiperItem
+      HomeSwiper
     },
     data() {
       return {
